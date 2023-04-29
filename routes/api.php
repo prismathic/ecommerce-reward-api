1771d3eb022edc5b4315e10d121a3a95da521eca
prismathic <pjjsalam1999@gmail.com>
2023-04-29 19:51:24 +0100

    add route for getting user achievements

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AchievementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('users')->name('users.')->group(function () {
+    Route::prefix('{user}')->group(function () {
+        Route::get('/achievements', [AchievementController::class, 'index'])->name('achievements.index');
+    });
 });
