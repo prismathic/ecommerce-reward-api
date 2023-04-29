d481ead80d13fdc8d594a875a729e0f123980557
prismathic <pjjsalam1999@gmail.com>
2023-04-29 11:46:41 +0100

    create basic table migrations and models

---
+++ database/migrations/2023_04_29_102524_create_user_achievements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_achievements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('user_id')->constrained();
+            $table->foreignUuid('achievement_id')->constrained();
+            $table->timestamp('unlocked_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_achievements');
+    }
+};
