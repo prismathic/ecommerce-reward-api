1532a5ed67ae62d310dd4389a320ac6a40891007
prismathic <pjjsalam1999@gmail.com>
2023-04-29 12:42:16 +0100

    create model/migration for storing cashback payment history

---
+++ database/migrations/2023_04_29_113835_create_cashback_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cashback_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained();
+            $table->decimal('amount', 13, 2, true);
+            $table->string('status');
+            $table->string('payment_client');
+            $table->string('reason');
+            $table->string('account_number');
+            $table->string('bank_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cashback_payments');
+    }
+};
