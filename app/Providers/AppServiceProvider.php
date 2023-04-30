59bb3094ea1dcfbf15eeb11638a4239b302e543a
prismathic <pjjsalam1999@gmail.com>
2023-04-30 18:50:19 +0100

    bind flutterwave to the payment client interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,21 @@
 
 namespace App\Providers;
 
+use App\Http\Clients\Flutterwave;
+use App\Http\Clients\PaymentClient;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * All of the container bindings that should be registered.
+     *
+     * @var array
+     */
+    public $bindings = [
+        PaymentClient::class => Flutterwave::class,
+    ];
+
     /**
      * Register any application services.
      *
