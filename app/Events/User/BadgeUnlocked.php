a71511111320366690eadda25ad0b8d85d2d4ff3
prismathic <pjjsalam1999@gmail.com>
2023-04-30 02:53:34 +0100

    add events for unlocking badges and achievement

---
+++ app/Events/User/BadgeUnlocked.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Events\User;
+
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class BadgeUnlocked implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public string $badgeName, public User $user)
+    {
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array|\Illuminate\Broadcasting\Channel
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel("users.{$this->user->id}");
+    }
+}
