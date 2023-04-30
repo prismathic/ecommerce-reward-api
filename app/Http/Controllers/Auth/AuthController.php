fe42ff5effe8da9a901d99ead26c4560b5ddbaf8
prismathic <pjjsalam1999@gmail.com>
2023-04-30 17:35:34 +0100

    complete work on auth logic

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Authenticate a user.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return $this->badRequestResponse('Invalid credentials.');
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->okResponse('Logged in successfully.', ['token' => $token]);
+    }
+
+    /**
+     * Log out an authenticated user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(): JsonResponse
+    {
+        auth()->user()->currentAccessToken()->delete();
+
+        return $this->okResponse('Logged out successfully.');
+    }
+}
